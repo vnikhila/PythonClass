b6f9d6f449bd582439df4799ea1a9109ebbda6e5
vnikhila <nikhila.venkitela@gmail.com>
2018-05-18 11:24:43 +0530

    Update

--- a.py
+++ a.py
@@ -1,9 +1,10 @@
-# import mainmod
-from mainmod import *
+# # import mainmod
+# from mainmod import *
 
-# print(a,b)
-# print(dir(mainmod))
+# # print(a,b)
+# # print(dir(mainmod))
 
-l = [1,2,3,4,'nikhi','hello']
-res = findindex(l,3)
-print(res)
+# l = [1,2,3,4,'nikhi','hello']
+# res = findindex(l,3)
+# print(res)
+
